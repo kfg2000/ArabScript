4eb7c7a023109f388e02a3fd1a869c44e1e7606b
Khalid Alghunaim <kfg2000@Khalids-MacBook-Pro.local>
2021-12-04 18:47:03 -0800

    compilers working

---
+++ src/arabScript.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import fs from "fs/promises"
+import process from "process"
+import compile from "./compiler.js"
+
+const help = `ArabScript compiler
+Syntax: src/arabScript.js <filename> <outputType>
+Prints to stdout according to <outputType>, which must be one of:
+    ast        the abstract syntax tree
+    analyzed   the semantically analyzed representation
+    generate   the translation to JavaScript
+`
+
+async function compileFromFile(filename, outputType) {
+  try {
+    const buffer = await fs.readFile(filename)
+    console.log(compile(buffer.toString(), outputType))
+  } catch (e) {
+    console.error(`${e}`)
+    process.exitCode = 1
+  }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
+}
---
+++ src/compiler.js
@@ -0,0 +1,28 @@
+// Compiler
+//
+// This module exports a single function
+//
+//   compile(sourceCodeString, outputType)
+//
+// The second argument tells the compiler what to return. It must be one of:
+//
+//   ast        the abstract syntax tree
+//   analyzed   the semantically analyzed representation
+//   js         the translation to JavaScript
+
+import parse from "./parser.js"
+import analyze from "./analyzer.js"
+import generate from "./generator.js"
+
+export default function compile(source, outputType) {
+  outputType = outputType.toLowerCase()
+  if (outputType === "ast") {
+    return parse(source)
+  } else if (outputType === "analyze") {
+    return analyze(parse(source))
+  } else if (outputType === "generate") {
+    return generate(analyze(parse(source)))
+  } else {
+    return "Unknown output type"
+  }
+}
